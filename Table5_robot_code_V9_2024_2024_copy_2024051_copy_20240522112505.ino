d92edbcd33fdb52ddf4d0db1970ad1584b41cf46
RandomWolf08 <devon6608@gmail.com>
2024-05-22 11:25:59 -0700

    V9
    Testing to see if the issue is our robot is being told to stop and go to much

---
+++ Table5_robot_code_V9_2024_2024_copy_2024051_copy_20240522112505.ino
@@ -0,0 +1,40 @@
+#include <PRIZM.h>  //Include all of the instructions for Tetrix stuff
+PRIZM robot;
+const int DRIVE = 50; // Forward and Backward speed
+const int SPIN = 100; // Spin speed
+const int STOP = 0; // STOP!!!
+
+
+void setup() {
+  // put your setup code here, to run once:
+  robot.PrizmBegin(); // Tells robot to start
+  robot.setMotorInvert(2,1); // (which motor, 1=yes/0=no)
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // while(robot.readSonicSensorCM(A3) >= 2.5){
+  //   robot.setMotorPowers(DRIVE,DRIVE); // (motor1, motor2) in percents
+  // }
+
+  if (robot.readSonicSensorCM(A3) < 2.5){
+
+  robot.setMotorTarget(1,SPIN,3600);
+  delay(3600);
+  robot.setMotorTarget(2,-SPIN,3600);
+  delay(3600);
+
+  robot.setMotorPowers(STOP,STOP);
+
+  // Servo 1 is 180º, Servo 2 is like 270º or something
+  robot.resetEncoder(1);
+  robot.resetEncoder(2);
+  //Still need to code Servo 2 (full movement) as well as need to fix the servo 1 after testing it.
+  } else {
+  robot.setMotorTarget(1,DRIVE, 3600);
+  delay(3600);
+  robot.setMotorTarget(2,DRIVE, 3600);
+  delay(3600);
+  }
+}
